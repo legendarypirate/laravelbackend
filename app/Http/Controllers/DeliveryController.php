11d3c6e55770815ede559ab0e8971ead42846bfb
legendarypirate <33424038+legendarypirate@users.noreply.github.com>
2023-07-08 16:12:38 +0800

    sq
    ss

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -6,6 +6,7 @@ use Yajra\DataTables\DataTables;
 use Illuminate\Http\Request;
 use App\Models\Delivery;
 use App\Models\Order;
+use App\Models\Good;
 
 class DeliveryController extends Controller
 {
@@ -39,6 +40,53 @@ class DeliveryController extends Controller
         $order->save();
         return redirect('/order/list')->with('message','Амжилттай хадгалагдлаа');
 
+    }
+
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+          
+        $cart = session()->get('cart', []);
+  
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name" => $product->name,
+                "quantity" => 1,
+                "price" => $product->price
+            ];
+        }
+          
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
+
+    public function update(Request $request)
+    {
+        if($request->id && $request->quantity){
+            $cart = session()->get('cart');
+            $cart[$request->id]["quantity"] = $request->quantity;
+            session()->put('cart', $cart);
+            session()->flash('success', 'Cart updated successfully');
+        }
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function remove(Request $request)
+    {
+        if($request->id) {
+            $cart = session()->get('cart');
+            if(isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product removed successfully');
+        }
     }
 
     public function list(){
---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,94 @@
+@extends('admin.master')
+
+@section('mainContent')
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          </div>
+          <div class="col-sm-6">
+           
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Хэрэглэгч үүсгэх</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              {!! Form::open(['url' => 'user/create', 'method'=>'post', 'role'=>'form', 'files' => true, 'enctype'=>'multipart/form-data' ]) !!}
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Нэр</label>
+                    <input type="text" name="name" class="form-control"  placeholder="Нэр">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Нууц үг</label>
+                    <input type="text" name="password" class="form-control"  placeholder="Нууц үг">
+                  </div>
+                  <div class="form-group">
+                        <label for="exampleSelectRounded0">Овор <code></code></label>
+                        <select class="custom-select rounded-0" id="exampleSelectRounded0">
+                        <option>1-5 kg</option>
+                        <option>5-10 kg</option>
+                        <option>10 kg<</option>
+                        </select>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </div>
+                {!! Form::close()!!}
+            </div>
+            <!-- /.card -->
+
+            <!-- general form elements -->
+        
+            <!-- /.card -->
+
+            <!-- Input addon -->
+        
+            <!-- /.card -->
+
+          </div>
+          <!--/.col (left) -->
+          <!-- right column -->
+          <div class="col-md-6">
+            <!-- Form Element sizes -->
+         
+            <!-- /.card -->
+
+        
+            <!-- /.card -->
+
+            <!-- general form elements disabled -->
+          
+            <!-- /.card -->
+            <!-- general form elements disabled -->
+            
+            <!-- /.card -->
+          </div>
+          <!--/.col (right) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+@endsection
