fac94b69ae33313cc0d3af346ddb5bdcc9962993
legendarypirate <33424038+legendarypirate@users.noreply.github.com>
2023-07-23 20:21:50 +0800

    s

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function () {
     Route::post('/login',[App\Http\Controllers\UserController::class, 'login']);
-    Route::get('/userlist',[App\Http\Controllers\UserController::class, 'userlist']);
-    Route::post('/addbonus',[App\Http\Controllers\UserController::class, 'addbonus']);
-    Route::post('/subbonus',[App\Http\Controllers\UserController::class, 'subbonus']);
-    Route::get('/totalbonus/{username}',[App\Http\Controllers\UserController::class, 'totalbonus']);
-    Route::get('/txnlist/{username}',[App\Http\Controllers\UserController::class, 'txnlist']);
-
+   
 });
